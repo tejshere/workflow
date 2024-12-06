aa1cd8d93b0a2b2a1a036b17522aa38fa08d81c1
Tejas Thorat <tejas@netomi.com>
2024-12-06 14:47:28 +0530

    Remove outdated "First Steps" link from homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,17 +18,6 @@ export default async function Home() {
           <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
             <Link
               className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
-              href="https://create.t3.gg/en/usage/first-steps"
-              target="_blank"
-            >
-              <h3 className="text-2xl font-bold">First Steps →</h3>
-              <div className="text-lg">
-                Just the basics - Everything you need to know to set up your
-                database and authentication.
-              </div>
-            </Link>
-            <Link
-              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
               href="https://create.t3.gg/en/introduction"
               target="_blank"
             >
