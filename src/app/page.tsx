638b3e45a6b8bc48e8e1f1d1e6d82f01fafe1fe1
Tejas Thorat <tejas@netomi.com>
2024-12-17 18:18:42 +0530

    fix: update loading message in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,7 @@ export default async function Home() {
 
           <div className="flex flex-col items-center gap-2">
             <p className="text-2xl text-white">
-              {hello ? hello.greeting : "Loading tRPC query..."}
+              {hello ? hello.greeting : "Loading asd tRPC query..."}
             </p>
           </div>
 
